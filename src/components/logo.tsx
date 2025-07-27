7e0ada77afdedd8fc7b15aeea0fbae8951512479
Alex Garcia <dadagasa27@gmail.com>
2025-07-27 21:19:41 +0000

    https://i.postimg.cc/J7N5r89y/LOGO-1.png
    reemplaza el logo por esta nue

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export function Logo({ className }: { className?: string }) {
   return (
     <Image 
-      src="https://i.postimg.cc/MTjtbjPt/LOGO.png" 
+      src="https://i.postimg.cc/J7N5r89y/LOGO-1.png" 
       alt="MiDoctorDeCasaApp Logo" 
       width={200} 
       height={150} 
