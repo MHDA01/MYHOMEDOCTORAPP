8f97347ba9672053c7227f31a4b2d2d889a635a0
Alex Garcia <dadagasa27@gmail.com>
2025-07-27 16:20:46 +0000

    aun no sale

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,15 +3,14 @@ import { cn } from '@/lib/utils';
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <div className={cn("flex flex-col items-center gap-2", className)}>
-      <Image 
-        src="https://storage.googleapis.com/monorepo-prod-project-resources/465d6830-1bfa-4458-8926-b07283626a57/my-home-doctor-logo.png" 
-        alt="MyHome DoctorApp Logo" 
-        width={200} 
-        height={150} 
-        data-ai-hint="app logo"
-        priority
-      />
-    </div>
+    <Image 
+      src="https://storage.googleapis.com/monorepo-prod-project-resources/465d6830-1bfa-4458-8926-b07283626a57/my-home-doctor-logo.png" 
+      alt="MyHome DoctorApp Logo" 
+      width={200} 
+      height={150} 
+      className={cn(className)}
+      data-ai-hint="app logo"
+      priority
+    />
   );
 }
