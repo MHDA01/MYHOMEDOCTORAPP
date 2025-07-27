ac2226f370f6464d1690d191138824ac2d8669e9
Alex Garcia <dadagasa27@gmail.com>
2025-07-27 17:21:01 +0000

    te falta mucho?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "@/components/ui/toaster"
 export const metadata: Metadata = {
   title: 'MiDoctorDeCasaApp',
   description: 'Tu asistente personal de gestión de salud.',
+  manifest: "/manifest.webmanifest",
 };
 
 export default function RootLayout({
@@ -19,6 +20,13 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet" />
+        <meta name="application-name" content="MiDoctorDeCasaApp" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="MiDoctorDeCasaApp" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#FFFFFF" />
       </head>
       <body className="font-body antialiased">
         {children}
