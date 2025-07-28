7ee1cda0c29c4630d6fb9695b29c4f6256be24cd
Alex Garcia <dadagasa27@gmail.com>
2025-07-28 12:57:00 +0000

    en este espacio de calendario de la fecha de nacimiento siguen saliendo

--- src/components/dashboard/personal-info.tsx
+++ src/components/dashboard/personal-info.tsx
@@ -111,7 +111,7 @@ export function PersonalInfo() {
             <div><strong>Sexo:</strong><p className="text-muted-foreground capitalize">{personalInfo.sex === 'male' ? 'Masculino' : personalInfo.sex === 'female' ? 'Femenino' : 'Indeterminado'}</p></div>
             <div>
                 <strong>Fecha de Nacimiento:</strong>
-                <p className="text-muted-foreground">{format(personalInfo.dateOfBirth, "d 'de' MMMM 'de' yyyy", { locale: es })} ({calculateAge(personalInfo.dateOfBirt_h)})</p>
+                <p className="text-muted-foreground">{format(personalInfo.dateOfBirth, "d 'de' MMMM 'de' yyyy", { locale: es })} ({calculateAge(new Date(personalInfo.dateOfBirth))})</p>
             </div>
             <div>
                 <strong>Previsión:</strong>
