2aaa58b5bec0418babb21dbdfa6388b9a0629bea
Alex Garcia <dadagasa27@gmail.com>
2025-08-12 04:08:39 +0000

    disminuye este espacio entre secciones

--- src/components/landing/hero-section.tsx
+++ src/components/landing/hero-section.tsx
@@ -5,11 +5,11 @@ import Image from 'next/image';
 
 export default function HeroSection() {
   return (
-    <section id="hero-section" className="bg-background pt-12 pb-16 md:pt-20 md:pb-20 text-center overflow-hidden w-full">
+    <section id="hero-section" className="bg-background pt-12 pb-12 text-center overflow-hidden w-full">
       <div className="w-full max-w-5xl mx-auto px-6">
         <div className="relative z-10 flex flex-col items-center">
             <div className="relative w-full max-w-xl mx-auto mb-8">
-              <div className="aspect-[4/3]">
+              <div className="aspect-[4/3] w-full max-w-5xl">
                  <Image 
                     src="https://i.postimg.cc/J7N5r89y/LOGO-1.png" 
                     alt="MyHomeDoctorApp Logo" 
