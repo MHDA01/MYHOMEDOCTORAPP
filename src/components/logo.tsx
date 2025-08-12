64cabebb1f3d3ade85b8b422192e57b0248b8bb3
Alex Garcia <dadagasa27@gmail.com>
2025-08-12 04:03:05 +0000

    ajusta el tamaño por favor

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,8 +5,8 @@ export function Logo({ className }: { className?: string }) {
     <Image 
       src="https://i.postimg.cc/J7N5r89y/LOGO-1.png" 
       alt="MyHomeDoctorApp Logo" 
-      width={200} 
-      height={150} 
+      width={160} 
+      height={120} 
       className={className}
       data-ai-hint="app logo"
       priority
