17de9292ba313eeb5e1ff6861f60776aa2806e54
Alex Garcia <dadagasa27@gmail.com>
2025-08-12 03:35:10 +0000

    ahora 404

---
+++ src/app/landing/page.tsx
@@ -0,0 +1,11 @@
+import { LandingPage } from '@/components/landing/landing-page';
+import Header from '@/components/landing/header';
+
+export default function Landing() {
+  return (
+    <>
+      <Header />
+      <LandingPage />
+    </>
+  );
+}
