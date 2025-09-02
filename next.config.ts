f232e0b69a1eec10df17bec468d1049ea0187158
MHDA01 <alejandrosainz.13@gmail.com>
2025-09-01 20:34:55 -0400

    feat: Restore to specified version with only cache-related changes.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type {NextConfig} from 'next';
 
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+});
+
 const nextConfig: NextConfig = {
+  output: 'export',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
